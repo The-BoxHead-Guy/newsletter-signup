8739dacaf6844382f0a3a3110838f9a4884a5e07
Jhon Alessandro <astralgmd@gmail.com>
2024-03-30 21:01:04 -0400

    fix: repair subscribe button event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 window.onload = () => {
-  const newsletter = document.getElementById("newsletter");
+  const newsletter = document.querySelector(".newsletter");
   const successSegment = document.getElementById("success");
   const successButton = document.querySelector(".success__button");
   const newsletterButton = document.querySelector(".newsletter__button");
 
   newsletterButton.addEventListener("click", () => {
+    newsletter.classList.remove("active");
     newsletter.classList.add("inactive");
     successSegment.classList.remove("inactive");
   });
