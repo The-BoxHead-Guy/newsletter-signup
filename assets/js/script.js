a13ea7c8bfa3ba5fd26d96552a79112d978911be
Jhon Alessandro <astralgmd@gmail.com>
2024-03-31 21:20:13 -0400

    feat: build the email validation and tab change after success message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,57 @@
 window.onload = () => {
+  // website elements
   const newsletter = document.querySelector(".newsletter");
   const successSegment = document.getElementById("success");
-  const successButton = document.querySelector(".success__button");
-  const newsletterButton = document.querySelector(".newsletter__button");
+  const dismissButton = document.getElementById("dismiss");
 
-  newsletterButton.addEventListener("click", () => {
-    newsletter.classList.remove("active");
-    newsletter.classList.add("inactive");
-    successSegment.classList.remove("inactive");
+  // Form elements
+  const form = document.querySelector("form");
+  const email = document.getElementById("email");
+  const error = document.querySelector(".email__error--message");
+
+  // Inline text elements
+  const successEmail = document.querySelector(".success__email");
+
+  form.addEventListener("submit", (e) => {
+    const REG_MAIL_TOKEN = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!email.value || email.value === null) {
+      // console.log("Please insert a valid email");
+      email.focus();
+      e.preventDefault();
+    } else if (email.value.match(REG_MAIL_TOKEN)) {
+      // console.log("Email is valid");
+      changeSuccessMessage(successEmail, email.value);
+      showSuccessMessage(newsletter, successSegment);
+      e.preventDefault();
+    } else {
+      console.log("Please insert a valid email");
+    }
+
+    e.preventDefault();
   });
 
-  successButton.addEventListener("click", () => {
-    successSegment.classList.add("inactive");
-    newsletter.classList.remove("inactive");
+  dismissButton.addEventListener("click", () => {
+    dismissMessage(newsletter, successSegment);
+    location.reload();
   });
 };
+
+/* This function will show the success message if the email is valid */
+const showSuccessMessage = (newsletter, success) => {
+  newsletter.style.display = "none";
+  success.style.display = "flex";
+};
+
+/* Changes the display of the success message */
+const changeSuccessMessage = (success, email) => {
+  success.innerHTML = email;
+};
+
+/* Dismiss success message */
+const dismissMessage = (newsletter, success) => {
+  success.style.display = "none";
+  newsletter.style.display = "flex";
+};
+
+// todo: Create the error messages if email is not valid
